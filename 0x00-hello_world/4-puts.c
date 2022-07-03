d54517014a839c5b3f76958f8012a920e382a056
abinetgobena <abinet.erm@gmail.com>
2022-07-03 13:50:08 -0700

    task-4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main -entry point
- * return:Always 0 (Success)
- * */
+ * main - prints the phrase "Programming is like building a multilingual puzzle"
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
-	puts("\"Programimg is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
         return (0);
 }
 
