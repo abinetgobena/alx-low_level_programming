b19f3789cf7b18cd1727dbe2c23a209c730e93a2
abinetgobena <abinet.erm@gmail.com>
2022-07-03 15:33:13 -0700

    task-5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdto.h>
+
+/**
+ * main - prints the phrase with proper grammsr,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, none zero otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
