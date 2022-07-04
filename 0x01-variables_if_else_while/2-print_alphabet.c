c27e396d4094cbf6aeff171245b2760c41eb964c
abinetgobena <abinet.erm@gmail.com>
2022-07-04 04:27:40 -0700

    task-2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>                                                                                                                     
+#include <time.h>                                                                                                                       
+#include <stdio.h>                                                                                                                      
+/**                                                                                                                                     
+ * main - prints the alphabet in lowercase  
+ * followed by a new line                                                                                                               
+ * Return: Always 0 (Success)                                                                                                           
+ */                                                                                                                                     
+int main(void)                                                                                                                          
+{                                                                                                                                       
+	        int ch;                                                                                                                         
+		        for (ch = 97; ch <= 122; ch++)                                                                                                  
+				        {                                                                                                                               
+						                putchar(ch);                                                                                                            
+								        }                                                                                                                               
+			        putchar(10);                                                                                                                    
+				        return (0);                                                                                                                     
+}                   
+
