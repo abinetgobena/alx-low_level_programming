5e13f2a852fb8f973bdc2dec30ef1b5195465747
abinetgobena <abinet.erm@gmail.com>
2022-07-04 16:43:00 -0700

    task=-9

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,8 +1,8 @@
 #include <stdio.h>                                                                                                                    
 /**                                                                                                                                   
-* main -> assign a random number to the variable n each time it is executed                                                          
-* and print the last digit of the number stored in the variable n                                                                    
-* Return : always 0                                                                                                                  
+ * main -> assign a random number to the variable n each time it is executed                                                          
+ * and print the last digit of the number stored in the variable n                                                                    
+* Return : always 0 (success)                                                                                                             
 */                                                                                                                                   
 int main(void)                                                                                                                        
 {                                                                                                                                     
@@ -32,5 +32,5 @@ int main(void)
 										        }                                                                                                                             
 					        putchar(10);                                                                                                                  
 						        return (0);                                                                                                                   
-}
+}      
 
